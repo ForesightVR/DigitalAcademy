b83c04333851c4f19a8a81878a5218b3c201e0ca
Sam Gebhard <33882552+sgebhard6@users.noreply.github.com>
2020-02-24 18:15:31 -0600

    Physics / Fun

---
+++ Assets/DriveTank.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveTank : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public Rigidbody tankPhysics;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+
+        tankPhysics.AddForce(transform.forward * Time.deltaTime * translation, ForceMode.Acceleration);
+        //transform.Translate(Vector3.forward * Time.deltaTime * translation);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotation);
+
+        Vector3 clampedCoords = new Vector3(Mathf.Clamp(transform.position.x, -8, 8), transform.position.y, transform.position.z);
+
+        transform.position = clampedCoords;
+
+        float distanceFromEdgeL = Vector3.Distance(transform.position, new Vector3(7.9f, transform.position.y, transform.position.z));
+        float distanceFromEdgeR = Vector3.Distance(transform.position, new Vector3(-7.9f, transform.position.y, transform.position.z));
+
+        if (distanceFromEdgeL <= 0.1f) { tankPhysics.velocity = (Vector3.zero); }
+
+        if (distanceFromEdgeR <= 0.1f) { tankPhysics.velocity = (Vector3.zero); }
+    }
+}
---
+++ Assets/flingObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flingObject : MonoBehaviour
+{
+    public float flingHeight = 50f;
+    public float flingDistance = 50f;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "obsticle")
+        {
+            Rigidbody obsticle = collision.transform.GetComponent<Rigidbody>();
+            obsticle.AddForce(0, flingHeight, flingDistance);
+        }
+    }
+}
